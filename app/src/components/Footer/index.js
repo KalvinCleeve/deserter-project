a17299ed681b599429499f0fc65d9683cf0ba2fa
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-20 15:27:57 +0200

    1er jet header/footer

--- app/src/components/Footer/index.js
+++ app/src/components/Footer/index.js
@@ -3,6 +3,7 @@
  */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /**
  * Local import
@@ -14,12 +15,20 @@ import React from 'react';
 
 const Footer = () => (
 
-  <footer>
-    <ul>
-      <li>DESERTER&copy; - 2018 - All Rights Reserved</li>
-      <li>CGU</li>
-      <li>Contact</li>
-    </ul>
+  <footer id="footer">
+    <div className="navbar is-fixed-bottom is-dark">
+      <div id="copy" className="navbar-start">
+        <span className="navbar-item">DESERTER&copy; - 2018 - All Rights Reserved</span>
+      </div>
+      <div id="link-footer" className="navbar-end">
+        <Link to="/cgu" className="navbar-item">
+          <span>CGU</span>
+        </Link>
+        <Link to="/contact" className="navbar-item">
+          <span>Contacter</span>
+        </Link>
+      </div>
+    </div>
   </footer>
 );
 
