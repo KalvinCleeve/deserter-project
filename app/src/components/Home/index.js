d960469234e368f5b45f2087ad03382dd7d0b7d6
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-23 15:33:53 +0200

    customisation de home

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -2,7 +2,6 @@
  * Npm import
  */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 /**
  * Local import
@@ -14,9 +13,16 @@ import PropTypes from 'prop-types';
  */
 const Home = () => (
   <div id="home">
-    <div id="titleHome">
-      <h1 id="mainTitle" className="animated bounceInDown">DESERTER</h1>
-      <h2 >The Game</h2>
+    <div id="intro">
+      <h1 id="mainTitle" className="animated bounceInDown">Deserter</h1>
+      <h2 id="subtitle">The Game</h2>
+      <div id="spitch">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis similique eum atque sapiente natus ut ducimus quasi, exercitationem fugiat eveniet cumque, error dolores tenetur magnam repudiandae ad laboriosam, deserunt ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <button className="button is-dark is-large animated bounce infinite">Jouer maintenant !</button>
+      </div>
     </div>
   </div>
 );
