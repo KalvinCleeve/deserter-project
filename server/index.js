3033565ddcf8b6a6f4187c02321010297e77610d
Arthur Beaucamp <arthur_beaucamp@laposte.net>
2018-04-23 12:07:09 +0200

    Connexion à la BDD et affichage du premier élèment de   Users

--- server/index.js
+++ server/index.js
@@ -1,30 +1,45 @@
 // Lance http de la biliothéque node.js qui créer le serveur web
 const http = require('http');
-// On récupère la page demandé par l'utilisateur
-const url = require('url');
+// Pour lié la page html
+const fs = require('fs');
+const { join } = require('path');
+
+// BDD
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017/deserter';
+
+// Path
+const indexPath = join(__dirname, '..', '/public/index.html');
 
 // Içi on lance le serveur web créer au dessus.
 // req correspond aux infos que le visiteur à demandé ( nom de la page, formulaire remplis ect...)
 // res correspond à l'object que l'on doit remplir pour transmettre un retour au visiteur.
 // Généralement res contient le code HTML à renvoyé
-const server = http.createServer((req, res) => {
-  // On récupère le nom de la page demandé
-  const page = url.parse(req.url).pathname;
-  console.log(page);
+const server = http.createServer();
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Test de Node.js</h1>');
+server.on('request', (request, response) => {
+  fs.readFile(indexPath, (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      response.end('Ce fichier n\'existe pas');
+    }
+    else {
+      response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      response.end(data);
+    }
+  });
 });
 
 // Connexion à la BDD
-const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
 
-MongoClient.connect('mongodb://localhost/deserter', (error, db) => {
-  if (error) return error;
-
-  console.log('Connecté à la base de données deserter');
-
-  return db;
+  const dbo = db.db('deserter');
+  dbo.collection('Users').findOne({}, (errorFind, result) => {
+    if (errorFind) throw errorFind;
+    console.log(result);
+    db.close();
+  });
 });
 
 // le server est lancé sur le port localhost:3000
