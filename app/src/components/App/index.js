1a599539e5652942d0333ece8aba1f2309202fa6
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-23 11:31:18 +0200

    màj 2.0 des routes

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -5,16 +5,23 @@ import React from 'react';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 import Home from 'src/components/Home';
+import Error404 from 'src/components/Error404';
 import Game from 'src/containers/Game';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 /* Code */
 const App = () => (
   <Router>
     <div id="app">
       <Header />
-      <Route path="/" component={Home} />
-      <Route path="/game" component={Game} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/game" component={Game} />
+        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route path="/cgu" component={cgu} /> */}
+        {/* <Route path="/contact" component={Contact} /> */}
+        <Route component={Error404} />
+      </Switch>
       <Footer />
     </div>
   </Router>
