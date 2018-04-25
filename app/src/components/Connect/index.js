185f5f438b0807e3a7d31b26deb4e92d2ab7021e
Arthur Beaucamp <arthur_beaucamp@laposte.net>
2018-04-25 15:11:27 +0200

    MiddleWare connect user

--- app/src/components/Connect/index.js
+++ app/src/components/Connect/index.js
@@ -11,17 +11,31 @@ import React from 'react';
 /**
  * Code
  */
-const Connect = () => (
-  <div id="connect">
-    <form className="form-connect has-text-centered" action="http://localhost:3000/connect" method="POST">
-      <input className="form-connect-input" type="email" name="email" placeholder="Votre Email" />
-      <input className="form-connect-input" type="password" name="password" placeholder="Votre Mot de passe" />
-      <button className="button is-dark form-connect-button" type="submit">Se connecter</button>
-    </form>
-  </div>
-);
+export default class Connect extends React.Component {
+  /**
+   * Actions
+   */
+  connectUser = (event) => {
+    event.preventDefault();
+    console.log('post envoyé');
+  };
+
+  /**
+   * Render
+   */
+  render() {
+    return (
+      <div id="connect">
+        <form className="form-connect has-text-centered" onSubmit={this.connectUser} action="http://localhost:3000/connect" method="POST">
+          <input className="form-connect-input" type="email" name="email" placeholder="Votre Email" />
+          <input className="form-connect-input" type="password" name="password" placeholder="Votre Mot de passe" />
+          <button className="button is-dark form-connect-button" type="submit">Se connecter</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 /**
  * Export
  */
-export default Connect;
