d3c70914dacc771c92f861bd77bb25098a64ca6f
luc-antoine <xyz_luc@hotmail.fr>
2018-04-25 23:41:25 +0200

    Interaction en partie fonctionnel (à finir et optimiser)

--- app/src/store/action.js
+++ app/src/store/action.js
@@ -22,19 +22,26 @@ const reducer = (state = initialState, action = {}) => {
           const position = action.heros.positionHeros.split('-');
           position[1] = Number(position[1]) + 1;
           position[0] = Number(position[0]);
-          // console.log(state.level1[position[0]][position[1]]);
-          // console.log([level1[5][3]]);
           if (state.level1[position[0]][position[1]].element === 'lever-off') {
-            // const aze = { ...state };
-            console.log(state.level1[position[0]][position[1]].element);
-            const aze = Object.assign(
-              {}, state,
-            // level1[position[0]][position[1]].element = 'lever-on',
-            );
-            console.log(aze);
-            console.log(state.level1[position[0]][position[1]].element);
+            const newlevel1 = [...state.level1];
+
+            newlevel1[position[0]][position[1]] = { element: 'lever-on', obstacle: true };
+
             return Object.assign({}, state, {
-              ...state,
+
+              level1: newlevel1,
+
+            });
+          }
+
+          if (state.level1[position[0]][position[1]].element === 'lever-on') {
+            const newlevel1 = [...state.level1];
+
+            newlevel1[position[0]][position[1]] = { element: 'lever-off', obstacle: true };
+
+            return Object.assign({}, state, {
+
+              level1: newlevel1,
 
             });
           }
