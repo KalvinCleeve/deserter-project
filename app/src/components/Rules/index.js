3c90bad87ee9045b7e5d38b83a294abbf1689084
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-26 14:24:02 +0200

    Ajout titre rules

--- app/src/components/Rules/index.js
+++ app/src/components/Rules/index.js
@@ -13,6 +13,7 @@ import React from 'react';
  */
 const Score = () => (
   <div id="rules" className="column">
+    <h3 className="title">RULES</h3><br />
     <span>Touches directionnelles pour déplacer le personnage</span><br /><br />
     <span>Touche espace pour interagir avec l'environnement</span><br /><br />
     <p>Le but du jeu sera de répondre à des énigmes et ainsi, avancer dans l’aventure jusqu’à rentrer chez soi, prêt à regarder Netflix !</p>
