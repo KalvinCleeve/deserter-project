cdb637c2ead2b682f6f969fc3646a26865f2d9c6
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-26 09:45:00 +0200

    correctifs divers

--- app/src/components/Score/index.js
+++ app/src/components/Score/index.js
@@ -15,15 +15,15 @@ const Score = () => (
   <div id="score" className="column">
     <div id="contentBoard" className="columns">
       <div id="placePlayer" className="column">
-        <h3 className="title is-6">Place</h3>
+        <h3>Place</h3>
         <span>1</span>
       </div>
-      <div id="namePlayer" className="column is-half">
-        <h3 className="title is-6">Joueur</h3>
+      <div id="namePlayer" className="column">
+        <h3>Joueur</h3>
         <span>Kalvin</span>
       </div>
       <div id="scorePlayer" className="column">
-        <h3 className="title is-6">Score</h3>
+        <h3>Score</h3>
         <span>100000</span>
       </div>
     </div>
