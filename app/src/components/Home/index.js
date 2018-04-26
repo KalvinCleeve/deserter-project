4a1aa09eaf8ed5efa4ac2453dae8113859f2275d
Kevin Le Calvé <lckevin44@gmail.com>
2018-04-26 13:50:24 +0200

    souci affichage dans /game

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -2,6 +2,7 @@
  * Npm import
  */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /**
  * Local import
@@ -19,9 +20,9 @@ const Home = () => (
         <p id="spitch-text"><i>“C’est la guerre, aux armes ! C’est la guerre, fuyez !”</i> <br /> Les gens, crient, hurlent, pleurent…<br /><br /> <strong>C’est la guerre.</strong><br /><br /> Les forces armées réunissent les personnes, homme comme femme, afin de les préparer pour la future boucherie qui va avoir lieu. Les trois puissances mondiales, Borëk, Goza et Taiyaki sont en conflits et nul ne sait pourquoi.<br /><br /> Je suis quelqu’un de banal, sans grande conviction… Et j’ai surtout une grande flemme de faire mumuse avec des flingues !
         </p>
       </div>
-      <div>
+      <Link to="/game">
         <button id="play-now" className="button is-dark is-large">Jouer maintenant !</button>
-      </div>
+      </Link>
     </div>
   </div>
 );
--- app/src/components/Rules/index.js
+++ app/src/components/Rules/index.js
@@ -13,8 +13,8 @@ import React from 'react';
  */
 const Score = () => (
   <div id="rules" className="column">
-    <span>Touches directionnelles pour déplacer le personnage</span><br />
-    <span>Touche espace pour interagir avec l'environnement</span>
+    <span>Touches directionnelles pour déplacer le personnage</span><br /><br />
+    <span>Touche espace pour interagir avec l'environnement</span><br /><br />
     <p>Le but du jeu sera de répondre à des énigmes et ainsi, avancer dans l’aventure jusqu’à rentrer chez soi, prêt à regarder Netflix !</p>
 
   </div>
