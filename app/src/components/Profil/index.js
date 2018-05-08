8c15a56707b0f83cd597ff7e904d82584ba09298
Gludus <rem.delemos@orange.fr>
2018-05-08 13:33:25 +0200

    Profile View

--- app/src/components/Profil/index.js
+++ app/src/components/Profil/index.js
@@ -14,17 +14,25 @@ import { Redirect } from 'react-router-dom';
  * Code
  */
 const Profil = ({ nickname, email, user }) => (
-  <div id="profil">
+  <div id="profile">
     { !user ? <Redirect to="/" /> : '' }
-    <h1 className="profil-title">{nickname}</h1>
-    <h2 className="profil-subtitle">Matricule</h2>
-    <p className="profil-email">{email}</p>
-    <h2 className="profil-subtitle">Fait d'armes</h2>
-    <ul className="profil-list">
-      <li className="profil-list-score">568pts le 01/03/2018</li>
-      <li className="profil-list-score">863pts le 25/03/2018</li>
-      <li className="profil-list-score">745pts le 17/04/2018</li>
-    </ul>
+    <h1>Departement of U.S Army</h1>
+    <div>
+      <h2 className="profile-title">recruit : </h2>
+      <p className="profile-recruit">{nickname}</p>
+    </div>
+    <div>
+      <h2 className="profile-subtitle">registration number :</h2>
+      <p className="profile-email">{email}</p>
+    </div>
+    <div>
+      <h2 className="profile-subtitle">military exploits :</h2>
+      <ul className="profile-list">
+        <li className="profile-list-score">Escaped 01/03/2018 with 321pts</li>
+        <li className="profile-list-score">Escaped 25/03/2018 with 8941pts</li>
+        <li className="profile-list-score">Escaped 17/04/2018 with 732pts</li>
+      </ul>
+    </div>
   </div>
 );
 Profil.propTypes = {
