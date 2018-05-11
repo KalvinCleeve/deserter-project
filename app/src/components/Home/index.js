6654a3a0244ebe5b18fb297a146fb5285c892117
Kevin Le Calvé <lckevin44@gmail.com>
2018-05-11 15:13:28 +0200

    translate correction

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -17,7 +17,7 @@ const Home = () => (
       <h1 id="mainTitle" className="animated bounceInDown is-large">Deserter</h1>
       <h2 id="subtitle">The Game</h2>
       <div id="spitch">
-        <p id="spitch-text"><i>"It's war, with arms! It's war, run away! "</i> <br /> People's screaming, crying, shrilling...<br /><br /> <strong>This is war.</strong><br /><br /> The armed forces bring people together, men and women, to prepare them for the future butchery that will take place. The three world powers, Borëk, Goza and Taiyaki are in conflict and no one knows why.<br /><br /> I am Mister Everyone, with not great convictions... And i prefer chill on Netflix.<br /> I need to get out here !
+        <p id="spitch-text"><i>"It's war, with arms! It's war, run away! "</i> <br /> People are screaming, crying, complaining...<br /><br /> <strong>This is war.</strong><br /><br /> The armed forces bring people together, men and women, to prepare them for the future butchery that will take place. The three world powers, Borëk, Goza and Taiyaki are in conflict and no one knows why.<br /><br /> I am Mister Everyone, with not great convictions... And i prefer chill on Netflix.<br /> I need to get out here !
         </p>
       </div>
       <button id="play-now" className="button is-dark is-large">Play Now !</button>
